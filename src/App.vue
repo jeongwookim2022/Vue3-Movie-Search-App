108a75627cb6f3921f761e5203d3d33eb2f8d9d6
jeongwookim2022 <jeongwookim2023@gmail.com>
2023-05-14 20:48:28 +0200

    Implemented Nav in Header Using Bootstrap nav-Pills

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
+  <Header />
   <RouterView />
 </template>
 
 
+<script>
+import Header from '~/components/Header.vue';
+  
+export default {
+  components: {
+    Header
+  },
+  data() {
+    
+  },
+}
+</script>
+
 <style lang="scss">
   @import "~/scss/main.scss";
+
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <div class="nav nav-pills">
+      <div
+        v-for="nav in navigations"
+        :key="nav.name"
+        class="nav-item">
+        <RouterLink
+          v-bind:to="nav.href"
+          active-class="active"
+          class="nav-link">
+          {{ nav.name }}
+        </RouterLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      navigations: [
+        {
+          name: 'Search',
+          href: '/'
+        },
+        {
+          name: 'Movies',
+          href: '/movie'
+        },
+        {
+          name: 'About',
+          href: '/about'
+        },
+      ]
+    }
+  },
+}
+</script>

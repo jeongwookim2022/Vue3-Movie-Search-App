4eaa9a8ecf11e9722a75e0fadff1b4579533ec49
jeongwookim2022 <jeongwookim2023@gmail.com>
2023-05-14 21:14:46 +0200

    Implemented Logo and Applied Google Fonts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <header>
+    <Logo />
     <div class="nav nav-pills">
       <div
         v-for="nav in navigations"
@@ -18,7 +19,12 @@
 
 
 <script>
+import Logo from "~/components/Logo.vue"
+
 export default {
+  components: {
+    Logo
+  },
   data() {
     return {
       navigations: [
@@ -39,3 +45,15 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+header {
+  height: 70px;
+  padding: 0 40px; // UP, Down X L, R OK
+  display: flex;
+  align-items: center;
+  .logo {
+    margin-right: 40px;
+  }
+}
+</style>

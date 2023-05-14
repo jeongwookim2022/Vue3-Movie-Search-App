817691835973ad226da59ac8cc61e10f6e734927
jeongwookim2022 <jeongwookim2023@gmail.com>
2023-05-14 19:00:10 +0200

    Set Bootstrap@5, Imported it in App.vue and Customized 'Variables defaults'

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
   <RouterView />
 </template>
+
+
+<style lang="scss">
+  @import "~/scss/main.scss";
+
+</style>
--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,3 +1,7 @@
 <template>
   <h1>Home</h1>
+  <div 
+    class="btn btn-primary">
+    Home
+    </div>
 </template>

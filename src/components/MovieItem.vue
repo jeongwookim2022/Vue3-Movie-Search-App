2d98980a62c9636fcd5b7712c1248dac9cce711b
jeongwookim2022 <jeongwookim2023@gmail.com>
2023-05-15 23:53:44 +0200

    Spreaded Movie Posters with Info when searched.

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>{{ movie.imdbID }}</div>
+  <div 
+    v-bind:style="{ backgroundImage: `url(${movie.Poster})` }"
+    class="movie">
+    <div class="info">
+      <div class="year">
+        {{ movie.Year }}
+      </div>
+      <div class="title">
+        {{ movie.Title }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  // "props"
   // Getting data From A Parent component 'MovieList.vue'
   // 1) If type is Object or Array, defaut should be a Function
   // - when you want to leave it empty.
@@ -15,3 +27,30 @@ export default {
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+
+.movie {
+  $width: 168px;
+  width: $width;
+  height: $width *(3/2);
+  margin: 10px;
+  border-radius: 4px;
+  background-color: $gray-400;
+  background-size: cover; // background-image covers the area of El with movie class.
+  overflow: hidden;
+  position: relative;
+  .info {
+    background-color: rgba($black, .3);
+    width: 100%;
+    padding: 14px;
+    font-size: 14px;
+    text-align: center;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+  }
+}
+</style>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -6,10 +6,12 @@
       </div>
       <!-- For v-bind:movie="movie", object data 'movie' is sent
            to 'MovieItem' as data named 'movie' next to ':'-->
-      <MovieItem
-      v-for="movie in movies"
-      :key="movie.imdbID"
-      :movie="movie" /> 
+      <div class="movies">
+        <MovieItem
+          v-for="movie in movies"
+          :key="movie.imdbID"
+          :movie="movie" /> 
+      </div>
     </div>
   </div>
 </template>
@@ -32,3 +34,14 @@ export default {
   }
 }
 </script>
+
+
+<style lang="scss">
+.container {
+  .movies {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+}
+</style>

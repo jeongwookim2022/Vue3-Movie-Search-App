95a828133d0b7334a5887693e8c7b2be3524395a
jeongwookim2022 <jeongwookim2023@gmail.com>
2023-05-16 21:37:35 +0200

    Divided Movie Detail page: 1) Skeleton with Loader After sending Request. 2) Movie Details After getting Resoponse. Used SCSS for details.

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -1,19 +1,65 @@
 <template>
   <div class="container">
-    <div class="skeletons">
-      <div class="skeleton poster"></div>
-      <div class="specs">
-        <div class="skeleton title"></div>
-        <div class="skeleton spec"></div>
-        <div class="skeleton plot"></div>
-        <div class="skeleton etc"></div>
-        <div class="skeleton etc"></div>
+    <template v-if="loading">
+      <!-- After Sending requsts -->
+      <!-- While waiting for Response --> 
+      <div class="skeletons">
+        <div class="skeleton poster"></div>
+        <div class="specs">
+          <div class="skeleton title"></div>
+          <div class="skeleton spec"></div>
+          <div class="skeleton plot"></div>
+          <div class="skeleton etc"></div>
+          <div class="skeleton etc"></div>
+        </div>
       </div>
-    </div>
     <Loader 
       v-bind:size="3"
       v-bind:z-index="9"
       v-bind:fixed="true" />
+    </template>
+
+
+    <!-- After getting Response -->
+    <div 
+      v-else
+      class="movie-details">
+      <div
+        v-bind:style="{ backgroundImage: `url(${theMovie.Poster})` }"
+        class="poster"></div>
+      <div class="specs">
+        <div class="title">
+          {{ theMovie.Title }}
+        </div>
+        <div class="labels">
+          <span>{{ theMovie.Released }}</span>
+          <span>{{ theMovie.Runtime }}</span>
+          <span>{{ theMovie.Country }}</span>
+        </div>
+        <div class="plot">
+          <span>{{ theMovie.Plot }}</span>
+        </div>
+        <div class="ratings">
+          <h3>Ratings</h3>
+        </div>
+        <div>
+          <h3>Actors</h3>
+          {{ theMovie.Actors }}
+        </div>
+        <div>
+          <h3>Director</h3>
+          {{ theMovie.Director }}
+        </div>
+        <div>
+          <h3>Production</h3>
+          {{ theMovie.Production }}
+        </div>
+        <div>
+          <h3>Genre</h3>
+          {{ theMovie.Genre }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,6 +71,15 @@ export default {
   components: {
     Loader
   },
+  computed: {
+    theMovie() {
+      return this.$store.state.movie.theMovie
+    },
+    loading() {
+      return this.$store.state.movie.loading
+    }
+  },
+
   // As soon as component is created.
   created() {
     this.$store.dispatch('movie/searchMovieWithId', {
@@ -74,6 +129,54 @@ export default {
       width: 50%;
       height: 50px;
       margin-top: 20px;
+    }
+  }
+}
+.movie-details {
+  display: flex; // Let Chidren Els be horizontal each other.
+  color: $gray-600;
+  .poster {
+    width: 500px;
+    height: 500px *3/2;
+    margin-right: 70px;
+    border-radius: 10px;
+    background-color: $gray-200;
+    background-size: cover; // Let Poster Img take all the space.
+    background-position: center;
+    flex-shrink: 0;
+  }
+  .specs {
+    flex-grow: 1;
+    .title {
+      color: $black;
+      font-family: 'Oswald', sans-serif;
+      font-size: 70px;
+      line-height: 1;
+      margin-bottom: 30px;
+    }
+    .labels {
+      color: $primary;
+      span {
+        &::after {
+          content: "\00b7";
+          margin: 0 6px;
+        }
+        &:last-child::after {
+          display: none;
+        }
+      }
+    }
+    .plot {
+      margin-top: 20px;
+    }
+    .ratings {
+
+    }
+    h3 {
+      margin: 24px 0 6px;
+      color: $black;
+      font-family: 'Oswald', sans-serif;
+      font-size: 20px;
     }
   }
 }

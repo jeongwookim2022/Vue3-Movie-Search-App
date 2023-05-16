61bf2a6a8b1b4feb7b3b21396d68df31f739dc47
jeongwookim2022 <jeongwookim2023@gmail.com>
2023-05-16 20:33:08 +0200

    Implemented Skeleton UI for Movie page.

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>Movie!</h1>
+  <div class="container">
+    <div class="skeletons">
+      <div class="skeleton poster"></div>
+      <div class="specs">
+        <div class="skeleton title"></div>
+        <div class="skeleton spec"></div>
+        <div class="skeleton plot"></div>
+        <div class="skeleton etc"></div>
+        <div class="skeleton etc"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
@@ -7,11 +18,54 @@
 export default {
   // As soon as component is created.
   created() {
-    console.log(this.$route)
-
     this.$store.dispatch('movie/searchMovieWithId', {
       id: this.$route.params.id
     })
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+
+.container {
+  padding-top: 40px;
+
+}
+.skeletons {
+  display: flex;
+  .poster {
+    flex-shrink: 0;
+    width: 500px;
+    height: 500px * 3/2;
+    margin-right: 70px;
+  }
+  .specs {
+    flex-grow: 1;
+  }
+  .skeleton {
+    border-radius: 10px;
+    background-color: $gray-200;
+    &.title {
+      width: 80%;
+      height: 70px;
+    }
+    &.spec {
+      width: 60%;
+      height: 30px;
+      margin-top: 20px;
+    }
+    &.plot {
+      width: 100%;
+      height: 250px;
+      margin-top: 20px;
+    }
+    &.etc {
+      width: 50%;
+      height: 50px;
+      margin-top: 20px;
+    }
+  }
+}
+</style>

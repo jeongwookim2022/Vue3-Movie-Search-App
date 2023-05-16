b3121d2fbda6db25eeeae0b9954bc08d157d13d9
jeongwookim2022 <jeongwookim2023@gmail.com>
2023-05-16 17:38:27 +0200

    Implemented Footer with Logo and Added it in App.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href="https://github.com/jeongwookim2022"
+      target="_blank">
+      (c){{new Date().getFullYear()}} KIM's
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: .3;
+  .logo {
+    display: block;
+    margin-bottom: 4px;
+  }
+  a {
+    text-decoration: none;
+  }
+}
+</style>

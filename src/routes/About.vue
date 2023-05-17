cc48235649f4833f560718ec0444c9be9219057b
jeongwookim2022 <jeongwookim2023@gmail.com>
2023-05-17 17:16:45 +0200

    Implemented About page with loadImage.js PlugIn(by making an img El in memory, inserting src in it and then adding EventLister to resolve() PlugIn) to show LoadingIcon until Image is loaded.

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,3 +1,99 @@
 <template>
-  <h1>About!</h1>
+  <div class="about">
+    <div class="photo">
+      <Lodader 
+        v-if="imageLoading"
+        absolute />
+      <img 
+        :src="image"
+        :alt="name">
+    </div>
+    <div class="name">
+      {{ name }}
+    </div>
+    <a
+      :href="`${blog}`"
+      class="blog">
+      {{ blog }}</a>
+    <div>{{ email }}</div>
+    <div>{{ phone }}</div>
+    <div>{{ address }}</div>
+  </div>
 </template>
+
+
+<script>
+import Loader from "~/components/Loader.vue"
+
+export default {
+  components: {
+    Loader
+  },
+  data() {
+    return {
+      imageLoading: true
+    }
+  },
+
+  computed: {
+    image() {
+      return this.$store.state.about.image
+    },
+    name() {
+      return this.$store.state.about.name
+    },
+    email() {
+      return this.$store.state.about.email
+    },
+    blog() {
+      return this.$store.state.about.blog
+    },
+    phone() {
+      return this.$store.state.about.phone
+    },
+    address() {
+      return this.$store.state.about.address
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    async init() {
+      await this.$loadImage(this.image)
+      this.imageLoading = false
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+@import "~/scss/main.scss";
+
+.about {
+  text-align: center;
+  .photo {
+    width: 250px;
+    height: 310px;
+    margin: 30px auto 15px;
+    padding: 40px;
+    border: 10px solid $gray-300;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-color: $gray-200;
+    position: relative;
+    img {
+      width: 100%;
+    }
+  }
+  .name {
+    font-size: 40px;
+    font-family: "Oswald", sans-serif;
+    margin-bottom: 20px;
+  }
+  .blog {
+    margin-bottom: 7px;
+  }
+}
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: "Jeongwo Kim",
+    blog: "https://github.com/jeongwookim2022",
+    email: "jeongwookim124068@gmail.com",
+    phone: "+46 0704914875",
+    image: "https://raw.githubusercontent.com/jeongwookim2022/src-vue-movie-app/main/kim.JPG",
+    address: "Åkersberga, Sweden"
+  })
+}
